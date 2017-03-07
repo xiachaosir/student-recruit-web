e2bf3ffca0746fa16c1e1585b7c778d428a81cda
xiachaosir <xiachaosir@163.com>
2017-03-07 18:22:41 +0800

    study interceptor exceptionHandler

---
+++ src/main/java/com/student/recruit/controller/MyException.java
@@ -0,0 +1,7 @@
+package com.student.recruit.controller;
+
+/**
+ * Created by xiachao on 2017/3/7.
+ */
+public class MyException {
+}
---
+++ src/main/java/com/student/recruit/interceptor/MyInterceptor.java
@@ -0,0 +1,7 @@
+package com.student.recruit.interceptor;
+
+/**
+ * Created by xiachao on 2017/3/6.
+ */
+public class MyInterceptor {
+}
